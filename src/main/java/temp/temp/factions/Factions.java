ee0781b0b83435d4a67004261a5188170af33bf2
Michael Ziluck <ziluckm@iastate.edu>
2018-01-12 01:08:17 -0600

    These are in the wrong place.

--- src/main/java/temp/temp/factions/Factions.java
+++ src/main/java/temp/temp/factions/Factions.java
@@ -1,17 +1,9 @@
 package temp.temp.factions;
 
-import java.util.Map;
-
 import org.bukkit.plugin.java.JavaPlugin;
-
-import temp.temp.factions.base.Faction;
-import temp.temp.factions.base.User;
 
 public class Factions extends JavaPlugin
 {
-
-    private Map<String, Faction> factions;
-    private Map<String, User> users;
 
     public void onEnable()
     {
