770c2fe04d4a1da7f165e1550eb8c927addc1ef6
Michael Ziluck <ziluckm@iastate.edu>
2019-06-29 14:58:58 -0500

    Documentation and code cleaning

--- src/main/java/com/ziluck/factions/data/mongodb/MongoWarp.java
+++ src/main/java/com/ziluck/factions/data/mongodb/MongoWarp.java
@@ -11,32 +11,27 @@ import com.ziluck.factions.spatial.LazyLocation;
  *
  * @author Michael Ziluck
  */
-@JsonIgnoreProperties({ "faction", "stub" })
-public final class MongoWarp extends LoadWarp
-{
+@JsonIgnoreProperties({ "stub" })
+public final class MongoWarp extends LoadWarp {
     /**
-     * An empty constructor. Jongo requires an empty constructor in order to load things.
-     * nicely.
+     * An empty constructor. Jongo requires an empty constructor in order to load things nicely.
      */
-    public MongoWarp()
-    {
+    public MongoWarp() {
     }
 
     /**
-     * Creates a new {@link Warp} with the given parameters. If locked is set to true, a {@link NullPointerException} will be
-     * thrown if the password is null.
+     * Creates a new {@link Warp} with the given parameters. If locked is set to true, a {@link NullPointerException}
+     * will be thrown if the password is null.
      *
      * @param name     the name of the warp.
      * @param location the location of the warp.
      * @param locked   whether this warp is password locked or not.
      * @param password the password for this warp.
      */
-    public MongoWarp(String name, LazyLocation location, boolean locked, String password)
-    {
+    public MongoWarp(String name, LazyLocation location, boolean locked, String password) {
         Preconditions.checkNotNull(name, "Name can't be null.");
         Preconditions.checkNotNull(location, "Location can't be null.");
-        if (locked)
-        {
+        if (locked) {
             Preconditions.checkNotNull(password, "Password can't be null if the warp is locked.");
         }
 
